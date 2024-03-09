389ee484ed976c6c9a0b6e63e4d2b67092723e3b
vishykaranth <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>
2024-03-09 07:17:35 +0530

    Spring JPA.

---
+++ springboot-jpa/src/main/java/com/spring/jpa/postgresql/model/Tutorial.java
@@ -0,0 +1,33 @@
+package com.spring.jpa.postgresql.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "published")
+  private boolean published;
+
+  public Tutorial(String title, String description, boolean published) {
+    this.title = title;
+    this.description = description;
+    this.published = published;
+  }
+
+}
